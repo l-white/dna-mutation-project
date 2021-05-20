8561a56e290d4a79b6d4afddbfae2193fda63b04
Laura <law7676@outlook.com>
2021-05-20 14:02:44 -0500

    Update main.js
    Started dnaCompare() function

--- main.js
+++ main.js
@@ -12,17 +12,18 @@ const mockUpStrand = () => {
     return newStrand;
 };
 
-const pAequorFactory = (number, array) => {
+const pAequorFactory = (number=1, array = mockUpStrand()) => {
     return {
         number: number,
         array: array,
         mutate() {
+            console.log(array);
             let newDnaBases = [];
             const returnNewDnaBase = newDnaBases => {
                 return newDnaBases[Math.floor(Math.random() * 3)];
             }
             // select DNA property
-            let dnaProperty = returnRandBase();
+            let dnaProperty = array[0];
             console.log(dnaProperty);
             // change the DNA property
             if (dnaProperty === 'A') {
@@ -37,6 +38,46 @@ const pAequorFactory = (number, array) => {
             dnaProperty = returnNewDnaBase(newDnaBases);
             array[0] = dnaProperty;
             return `dnaProperty: ${dnaProperty}, array: ${array}`;
-        } 
+        },
+        compareDNA(newPAequor) {
+            newPAequor = pAequorFactory();
+            let newArray = [];
+            /*
+            if (array.length !== pAequorFactory.array.length) {
+                if (array.length > pAequorFactory.array.length) {
+
+                }
+            }
+            */
+            for (let i = 0; i < array.length; i++) {
+                for (let j = 0; j < newPAequor.array.length; j++) {
+                    if (array[i] === newPAequor.array[j]) {
+                        newArray.push(newPAequor.array[j]);
+                    }
+                }
+            }
+            return newArray;
+        }
     }
 }; 
+
+
+
+
+const firstFactory = pAequorFactory(2, ['A', 'T', 'C', 'C']);
+//console.log(firstFactory.mutate());
+//console.log(firstFactory.number);
+
+console.log(firstFactory.mutate());
+console.log(firstFactory.compareDNA());
+//console.log(returnRandBase());
+//console.log(mockUpStrand());
+//const secondFactory = pAequorFactory(3);
+//console.log(secondFactory.mutate());
+
+const array1 = ['a', 'b', 'c', 'd'];
+const array2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+const getDiff = array2.length - array1.length;
+console.log(getDiff);
+console.log(array2.slice(0, getDiff + 1));
+console.log(array1);
