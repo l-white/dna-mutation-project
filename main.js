b19aa82e232899a4d1afbe5bd7292e98e196fefb
Laura <law7676@outlook.com>
2021-05-20 10:19:09 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G'];
+    return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+    const newStrand = [];
+    for (let i = 0; i < 15; i++) {
+        newStrand.push(returnRandBase());
+    }
+    return newStrand;
+};
+
+const pAequorFactory = (number, array) => {
+    return {
+        number: number,
+        array: array,
+        mutate() {
+            return returnRandBase();
+        }
+    }
+}; 
